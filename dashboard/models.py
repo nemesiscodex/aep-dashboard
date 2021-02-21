58caf94629daad28b4b0099d8911628fea28d4d0
Julio Daniel Reyes <nemesiscodex@gmail.com>
2021-02-20 21:00:04 -0300

    Data model

--- dashboard/models.py
+++ dashboard/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from enum import Enum
 
 class Location(models.Model):
-    latitude = models.CharField(max_length=200)
-    longitude = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
 
     class Meta:
         abstract = True
@@ -18,22 +18,46 @@ class AreaType(ChoiceEnum):
 
 
 # Models
-class Area(Location):
+class Area(models.Model):
     name = models.CharField(max_length=200)
     area_type = models.CharField(max_length=16, choices=AreaType.choices())
-    parent_area = models.ForeignKey(
-        'self',
-        on_delete=models.SET_NULL,
-        null=True
-    )
+    # TODO: polygon type
 
-class SensorType(ChoiceEnum):
+    class Meta:
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['area_type'])
+        ]
+
+class DetectorType(ChoiceEnum):
     KissingBug, = range(1)
 
-class Sensor(Location):
-    sensor_type = models.CharField(max_length=16, choices=SensorType.choices())
-    area = models.ForeignKey(
-        Area,
-        on_delete=models.SET_NULL,
-        null=True
-    )
+class Detector(Location):
+    detector_type = models.CharField(max_length=16, choices=DetectorType.choices())
+    identifier = models.CharField(max_length=16, null=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['identifier']),
+            models.Index(fields=['detector_type'])
+        ]
+
+class Activation(models.Model):
+    timestamp = models.DateTimeField()
+    detector_identifier = models.CharField(max_length=16, null=False)
+    uptime_days = models.IntegerField()
+    uptime_hours = models.IntegerField()
+    uptime_minutes = models.IntegerField()
+    uptime_seconds = models.IntegerField()
+    sensor_1 = models.IntegerField()
+    sensor_2 = models.IntegerField()
+    sensor_3 = models.IntegerField()
+    sensor_4 = models.IntegerField()
+    count = models.IntegerField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['detector_identifier']),
+            models.Index(fields=['count']),
+            models.Index(fields=['timestamp'])
+        ]
